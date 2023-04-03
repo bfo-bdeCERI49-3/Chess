ae2c5e1ebb0aa04bb350bc7c403fa79f8cb9b4ad
DarsFabian <dars.fabian@gmail.com>
2023-04-04 00:35:57 +0200

    Created Move and Board classes

---
+++ src/board/board.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "../pieces/BasePiece.hpp"
+
+class Move {
+    public:
+        Coordinates originCoordinates;      // Starting position of the move
+        Coordinates destinationCoordinates; // Ending position of the move
+};
+
+class Board {
+    private:
+        /**
+         * 2D Grid representing the checker
+        */
+        BasePiece* grid[8][8] { nullptr };
+
+        /**
+         * Grid representing all available moves for White player
+        */
+       Move* whiteAvailableMoves { nullptr };
+
+       /**
+        * Grid representing all available moves for Black player
+       */
+        Move* blackAvailableMoves { nullptr };
+
+        /**
+         * 0 if white player's turn, 1 if black player's turn
+        */
+        unsigned char playerTurn { 0 };
+
+    public:
+        /**
+         * CONSTRUCTOR
+        */
+        Board();
+
+        /**
+         * Returns the piece at coordinates (x,y) or nullptr if no piece is present
+         * @return BasePiece* or nullptr
+        */
+        inline BasePiece* get(size_t x, size_t y) { return this->grid[x][y]; };
+
+        /**
+         * Get all of White player's available moves
+         * @return Move*
+        */
+        inline Move* getWhiteAvailableMoves() { return this->whiteAvailableMoves; };
+
+        /**
+         * Get all of Black player's available moves
+         * @return Move*
+        */
+        inline Move* getBlackAvailableMoves() { return this->blackAvailableMoves; };
+
+        /**
+         * Returns which player's turn it is - 1 if black's turn, 0 if white's turn
+         * @return unsigned char
+        */
+        inline unsigned char getTurn() { return this->playerTurn; };
+};
