020d5425216735d26d1eda04df0aae852cf2e588
DarsFabian <dars.fabian@gmail.com>
2023-04-04 23:31:35 +0200

    Created Pawn header file

---
+++ src/pieces/pawn.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "./BasePiece.hpp"
+#include "../board/board.hpp"
+
+class Pawn : public BasePiece {
+    public:
+        /**
+         * Constructors
+        */
+        Pawn(Color&, Coordinates&);
+        Pawn(Color&, unsigned char, unsigned char);
+
+        /**
+         * Evaluates possible movements for said piece and stores it in the available moves for said team
+         * @return void
+        */
+        void evaluateMovements(Board&);
+};
