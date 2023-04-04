bc89133da390b6ea5fe4d201a82bda59a8c6707d
DarsFabian <dars.fabian@gmail.com>
2023-04-04 23:35:48 +0200

    Created Knight header file

---
+++ src/pieces/knight.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "./BasePiece.hpp"
+#include "../board/board.hpp"
+
+class Knight : public BasePiece {
+    public:
+        /**
+         * Constructors
+        */
+        Knight(Color&, Coordinates&);
+        Knight(Color&, unsigned char, unsigned char);
+
+        /**
+         * Evaluates possible movements for said piece and stores it in the available moves for said team
+         * @return void
+        */
+        void evaluateMovements(Board&);
+};
